a4bd2ddb5a4cd629a3146cd53d4559f80c4dce20
UnidentifiedRaccoon <yura.posledov@yandex.ru>
2021-09-29 00:57:27 +0300

    Переделал календарь

--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -1,79 +1,121 @@
 import './calendar.scss';
-import 'air-datepicker/dist/css/datepicker.min.css';
-import 'air-datepicker/dist/js/datepicker.min.js';
 
-let index = document.querySelector('.index')
-let catalog = document.querySelector('.catalog');
-let signUp = document.querySelector('.sign-up')
+// Если у вас "убегает" календарь - закомментируйте строчки в datepicker.js
+// set date (val) { - в этом сэттере запретить изменение координат
+//.............................................
+        // if (this.visible && this.elIsInput) {
+        //     this.setPosition();
+        // }
+//.............................................
+
+
+// set view (val) { - в этом сэттере запретить изменение координат
+//.............................................
+// if (this.elIsInput && this.visible) this.setPosition();
+//.............................................
+
+import 'air-datepicker/dist/js/datepicker.js';
+import 'air-datepicker/dist/css/datepicker.min.css';
+
+
+const firstInputClass = '.calendar__input--start'
+const secondInputClass = '.calendar__input--end'
+
+function setSecondInput(inputFinish) {
+    function bindKeyboardEvents() {
+        inputFinish.on('keydown.adp', this._onKeyDown.bind(this));
+        inputFinish.on('keyup.adp', this._onKeyUp.bind(this));
+        inputFinish.on('hotKey.adp', this._onHotKey.bind(this));
+    }
+    function focusHandler() {
+        currentDatepicker.$datepicker[0].classList.add('active')
+        currentDatepicker.$datepicker[0].style.left = '0px'
+        currentDatepicker.$datepicker[0].style.top = currentDatepicker.el.closest('.calendar-box').offsetHeight + 12 + 'px'
+    }
+
+    let currentDatepicker = $(firstInputClass).datepicker().data('datepicker');
+
+    inputFinish.on('focus', focusHandler)
+    inputFinish.on('blur', currentDatepicker.hide.bind(currentDatepicker))
+    bindKeyboardEvents.bind(currentDatepicker)()
+}
 
 let options = {
-    classes: "calendar",
-    prevHtml:'<svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16.1757 8.01562V9.98438H3.98819L9.56632 15.6094L8.16007 17.0156L0.144441 9L8.16007 0.984375L9.56632 2.39062L3.98819 8.01562H16.1757Z" fill="#BC9CFF"/></svg>',
-    nextHtml:'<svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.36252 0.984375L16.3781 9L8.36252 17.0156L6.95627 15.6094L12.5344 9.98438H0.346894V8.01562H12.5344L6.95627 2.39062L8.36252 0.984375Z" fill="#BC9CFF"/></svg>',
-    // autoClose: true,
+    prevHtml: '<span class="calendar__prev">arrow_back</span>',
+    nextHtml: '<span class="calendar__next">arrow_forward</span>',
+    classes: 'calendar',
     clearButton: true,
+    navTitles: { days: 'MM <i>yyyy</i>' },
     // inline: true,
-
+    onShow(inst, animationCompleted) {
+        inst.$datepicker[0].style.left = '0px'
+        inst.$datepicker[0].style.top = inst.el.closest('.calendar-box').offsetHeight + 12 + 'px'
+    },
 }
 
-if (index) {
-    $(document).ready(function () {
-
-        options.minDate = new Date()
-        options.range = true
-        options.multipleDatesSeparator = ' - '
-        options.onSelect = function (fd) {
-            $('.js--calendar-field--start').val(fd.split("-")[0]);
-            $('.js--calendar-field--end').val(fd.split("-")[1]);
+// Конфигурация для каждого из календарей может отличаться от стандартной
+// Настроим эти конфигурации
+let mode = document.querySelector('.calendar-box').dataset.mode;
+console.log(mode)
+function onRenderCell(date, cellType) {
+    if (cellType === 'day' && date.getDate() === options.minDate.getDate()) {
+        return {
+            disabled: true
         }
-
-        $('.js--calendar-field--start').datepicker(options);
-    })
-}
-
-if (catalog) {
-    $(document).ready(function () {
-
-        options.minDate = new Date()
-        options.range = true
-        options.dateFormat = 'd, M'
-        options.onSelect = function (fd) {
-            let arr = fd.split(",")
-            let row
-            if (arr.length === 2) row = `${arr[0]} ${arr[1]}`
-            else if ((arr.length === 4)) row = `${arr[0]} ${arr[1]} - ${arr[2]} ${arr[3]}`
-            else row = fd
-
-            $('.js--calendar-field--start').val(row);
-        }
-
-        $('.js--calendar-field--start').datepicker(options);
-    })
-}
-
-if (signUp) {
-    $(document).ready(function () {
-        $('.js--calendar-field--start').datepicker(options);
-    })
-}
-
-
-// Находит календарь, добавляет в него кнопку apply,
-// навешивает на неё обработчик handler = hide - позаимствован из библиотеки
-let intervalId = setInterval(() => {
-    let calendar = document.querySelector('.calendar');
-    if (calendar) {
-        let currentDatepicker = $('.js--calendar-field--start').datepicker().data('datepicker')
-        let buttonsWrapper = calendar.querySelector('.datepicker--buttons');
-        let applyButton = document.createElement('span');
-        applyButton.classList.add('datepicker--button');
-        applyButton.textContent = 'Применить'
-        applyButton.addEventListener('click', currentDatepicker.hide)
-        buttonsWrapper.appendChild(applyButton);
-        if (catalog) calendar.classList.add('js--filter-calendar');
-        clearInterval(intervalId)
     }
-}, 500)
+}
+
+if (mode === "index") {
+    options.minDate = new Date()
+    options.range = true
+    options.onRenderCell = onRenderCell
+    options.multipleDatesSeparator = ' - '
+    options.onSelect = function (fd) {
+        $(firstInputClass).val(fd.split("-")[0]);
+        $(secondInputClass).val(fd.split("-")[1]);
+    }
+}
+
+if (mode === "catalog") {
+    options.minDate = new Date()
+    options.range = true
+    options.dateFormat = 'd, M'
+    options.onRenderCell = onRenderCell
+    options.onSelect = function (fd) {
+        let arr = fd.split(",")
+        let row
+        if (arr.length === 2) row = `${arr[0]} ${arr[1]}`
+        else if ((arr.length === 4)) row = `${arr[0]} ${arr[1]} - ${arr[2]} ${arr[3]}`
+        else row = fd
+
+        $(firstInputClass).val(row);
+    }
+}
+
+
+$(function() {
+    // Инициализируем календарь на первом input
+    $(firstInputClass).datepicker(options)
+
+    // Если у нас календарь предполагает использование
+    // второго поля input, то добавляем и его
+    let inputFinish = $(secondInputClass)
+    if (inputFinish) {
+        setSecondInput(inputFinish)
+    }
+
+    // Вставляем календарь непосредственно в calendarBox (по дефолту от встраивается в конец body)
+    let calendarBox = $('.calendar-box')
+    let datepickerContainer = $('.datepickers-container')
+    calendarBox.append(datepickerContainer)
+
+
+    // Добавляем кнопку принять
+    let buttonsWrapper = $('.datepicker--buttons');
+    let applyButton = `<span class="datepicker--button" data-action="hide">Применить</span>`
+    buttonsWrapper.append(applyButton);
+});
+
 
 
 

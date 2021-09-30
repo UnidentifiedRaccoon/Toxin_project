6d843f5123295da7be3bfa29f206a6fe808bb261
UnidentifiedRaccoon <yura.posledov@yandex.ru>
2021-09-30 20:42:51 +0300

    Responsive верстка catalog/room-page-template. Блоки filter/room-card

--- src/entry.js
+++ src/entry.js
@@ -5,7 +5,10 @@ function requireAll(requireContext) {
     return requireContext.keys().map(requireContext);
 }
 
-requireAll(require.context('./components', true, /^\.\/(?!.*(?:__tests__)).*\.(jsx?)$/));  // pattern to take each .js(x) files except of the ones with __tests__ directory https://regex101.com/r/J8NWTj/1
-requireAll(require.context('./pages', true, /^\.\/(?!.*(?:__tests__)).*\.(jsx?)$/));
+requireAll(require.context('./components', true, /\.(jsx?)$/));  // pattern to take each .js(x) files except of the ones with __tests__ directory https://regex101.com/r/J8NWTj/1
+requireAll(require.context('./pages', true, /\.(jsx?)$/));
+requireAll(require.context('./components', true, /\.(scss?)$/));
+requireAll(require.context('./pages', true, /\.(scss?)$/));
+
 
 
--- src/pages/catalog/catalog.js
+++ src/pages/catalog/catalog.js
@@ -1,2 +1,13 @@
 import './catalog.scss'
 
+let filterButton = document.querySelector('.catalog__filter-toggle')
+
+let filter = document.querySelector('.catalog__wrapper-left')
+
+if (filterButton) {
+    filterButton.addEventListener('click', () => {
+        filter.classList.toggle('active')
+        filterButton.classList.toggle('active')
+    })
+}
+

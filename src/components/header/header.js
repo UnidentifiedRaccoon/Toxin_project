b69a1c711244f643c620af9e1812ce62c776be64
UnidentifiedRaccoon <yura.posledov@yandex.ru>
2021-09-30 20:40:59 +0300

    Responsive верстка index/sign-in/sign-up. Блоки header/footer/modal/main-nav/slider/link

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,1 +1,12 @@
 import './header.scss';
+
+let menuButton = document.querySelector('.header__menu-toggle')
+
+let menu = document.querySelector('.header__menu-wrapper')
+
+if (menuButton) {
+    menuButton.addEventListener('click', () => {
+        menu.classList.toggle('active')
+        menuButton.classList.toggle('active')
+    })
+}
